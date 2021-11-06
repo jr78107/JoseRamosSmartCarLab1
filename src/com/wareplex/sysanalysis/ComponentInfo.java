f6ef07552bca7fa49779998c6e055acbdb64f4d4
jenriqueramos <jeramos@comcast.net>
2021-11-05 22:36:40 -0400

    Commit failed

--- src/com/wareplex/sysanalysis/ComponentInfo.java
+++ src/com/wareplex/sysanalysis/ComponentInfo.java
@@ -12,5 +12,17 @@ public class ComponentInfo {
 	private String compModel;
 	private String compVersion;
 	private String compType;
+	/**
+	 * @return the compModel
+	 */
+	public String getCompModel() {
+		return compModel;
+	}
+	/**
+	 * @param compModel the compModel to set
+	 */
+	public void setCompModel(String compModel) {
+		this.compModel = compModel;
+	}
 
 }
