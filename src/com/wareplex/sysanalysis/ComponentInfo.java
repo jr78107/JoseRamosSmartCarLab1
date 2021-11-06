2d2413041043f4bc7f25ad3cfe04b70e7d35b76b
jenriqueramos <jeramos@comcast.net>
2021-11-05 22:35:44 -0400

    Test commit

---
+++ src/com/wareplex/sysanalysis/ComponentInfo.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.wareplex.sysanalysis;
+
+/**
+ * @author Jose
+ *
+ */
+public class ComponentInfo {
+	
+	private String compModel;
+	private String compVersion;
+	private String compType;
+
+}
